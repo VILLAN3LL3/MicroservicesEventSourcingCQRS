d813f4afb18d5ab90eac2aada9804885d1f58fd9
VILLAN3LL3 <development@gutfrau.de>
2024-03-09 20:12:03 +0100

    added h2 database connection

---
+++ ProductsService/src/main/java/com/villan3ll3/estore/ProductsService/core/data/ProductsRepository.java
@@ -0,0 +1,9 @@
+package com.villan3ll3.estore.ProductsService.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<ProductEntity, String> {
+
+    ProductEntity findByProductId(String productId);
+    ProductEntity findByProductIdOrTitle(String productId, String title);
+}
---
+++ ProductsService/src/main/java/com/villan3ll3/estore/ProductsService/query/ProductEventsHandler.java
@@ -0,0 +1,27 @@
+package com.villan3ll3.estore.ProductsService.query;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import com.villan3ll3.estore.ProductsService.core.data.ProductEntity;
+import com.villan3ll3.estore.ProductsService.core.data.ProductsRepository;
+import com.villan3ll3.estore.ProductsService.core.events.ProductCreatedEvent;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProductEventsHandler {
+
+    private final ProductsRepository repository;
+
+    @EventHandler
+    public void on(ProductCreatedEvent event) {
+        
+        ProductEntity entity = new ProductEntity();
+        BeanUtils.copyProperties(event, entity);
+
+        repository.save(entity);
+    }
+}
